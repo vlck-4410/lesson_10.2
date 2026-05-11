c7564eba66532bc54ce29c7bf880100e11db9f88
Zarubin Sergey <seregazarubin2003@mail.ru>
2026-05-11 12:23:18 +1000

    First task

---
+++ src/main.py
@@ -0,0 +1,18 @@
+def add_numbers(a, b):
+    return a + b
+
+def is_even(a):
+
+    return a % 2 == 0
+
+def find_max(my_list):
+    if len(my_list) > 0:
+        return max(my_list)
+    return 0
+
+if __name__ == '__main__':
+    assert add_numbers(2,  2) == 4
+
+    assert is_even(0) == True
+
+    assert find_max([]) == 0
