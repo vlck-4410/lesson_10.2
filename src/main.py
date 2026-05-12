e2bcee17be5816f4058dc0fcf3a233d487238ecf
Zarubin Sergey <seregazarubin2003@mail.ru>
2026-05-12 17:14:01 +1000

    add def calc_avg

--- src/main.py
+++ src/main.py
@@ -31,3 +31,15 @@ def calculate_logorifm(x, base):
 
 def reverse_string(my_string):
    return my_string[::-1]
+
+
+def calc_avg(my_list):
+    if len(my_list) > 0:
+        return sum(my_list) / len(my_list)
+    return 0
+
+
+if __name__ == '__main__':
+    assert calc_avg([1, 2, 3, 4]) == 2.5
+
+    assert calc_avg([]) == 0
